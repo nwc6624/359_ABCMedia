71094e1de3077166758f850f00cece6a6341e7db
Noah <nwc6624@gmail.com>
2023-04-02 11:31:31 -0600

    Update proj.py

--- Part 3/proj.py
+++ Part 3/proj.py
@@ -1,3 +1,18 @@
+def create_connection(db_file):      #https://www.sqlitetutorial.net/sqlite-python/creating-tables/
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+
+
 
 if __name__ == '__main__':
     main()
